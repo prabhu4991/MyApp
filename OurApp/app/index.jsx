884b0dc9e810790c3f9609cc90f020668f02e0f8
prabhu4991 <127617644+prabhu4991@users.noreply.github.com>
2024-07-12 09:44:25 +0530

    today

--- OurApp/app/index.jsx
+++ OurApp/app/index.jsx
@@ -1,13 +1,13 @@
+import { useState } from "react";
 import {
   ImageBackground,
   Pressable,
   ScrollView,
   Text
 } from "react-native";
-import image from "../assets/images/photo.jpeg";
+import ViewPager from "react-native-pager-view";
 import image1 from "../assets/images/bike2.jpg";
-import { useState } from "react";
-
+import image from "../assets/images/photo.jpeg";
 export default function Task() {
   const [color, setColor] = useState(false);
 
@@ -18,50 +18,20 @@ export default function Task() {
 
 
     <ScrollView>
-      
+
       <ImageBackground source={image} resizeMode="cover">
         <ImageBackground source={image1} resizeMode="cover">
-        <Pressable onPress={() => handleColor()}>
-          <Text
-            style={{
-              fontSize: 50,
-              color: "yellow",
-            }}
-          >
-            hello
-          </Text>
-        </Pressable>
+          <Pressable onPress={() => handleColor()}>
+            <Text
+              style={{
+                fontSize: 50,
+                color: "yellow",
+              }}
+            >
+              hello
+            </Text>
+          </Pressable>
 
-        <Text
-          style={{
-            textAlign: "center",
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "red",
-          }}
-        >
-          GoodMorning
-        </Text>
-        <Text
-          style={{
-            textAlign: "center",
-            fontSize: 78,
-            fontWeight: "condensed",
-            color: "skyblue",
-          }}
-        >
-          आहिस्ता चल ज़िन्दगी, अभी कई क़र्ज़ चुकाना बाकी है, कुछ दर्द मिटाना बाकी
-          है, कुछ फ़र्ज़ निभाना बाकी है!!. hiii
-        </Text>
-      </ImageBackground>
-      </ImageBackground>
-
-      {color &&{(
-        <ImageBackground source={image} resizeMode="cover"> 
-         <Pressable onPress={()=> handleColor()}>
-          <Text>hello</Text>
-        </Pressable>
-     ) }}
           <Text
             style={{
               textAlign: "center",
@@ -84,8 +54,16 @@ export default function Task() {
             है, कुछ फ़र्ज़ निभाना बाकी है!!. hiii
           </Text>
         </ImageBackground>
+      </ImageBackground>
+<ViewPager  style={Styles.container} initalPage={0} ref={page} >
+<View style={({...styles.page}, {backgroundColor:"red"})} key="1">
+  
 
+
+
+ 
+</ViewPager>
     </ScrollView>
-    
+
   );
 }
