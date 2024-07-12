17e4bedf790320fc16348f7f3b07ee144ce3cf33
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-07-12 17:56:05 +0530

    Today Task

--- OurApp/app/index.jsx
+++ OurApp/app/index.jsx
@@ -1,92 +1,91 @@
-// import { Link } from "expo-router";
-import { Link } from "expo-router";
-import { Dimensions, StyleSheet, Text, View,Image } from "react-native";
-import MapView, { Marker, Polyline } from "react-native-maps";
+import {
+  ImageBackground,
+  Pressable,
+  ScrollView,
+  Text
+} from "react-native";
+import image from "../assets/images/photo.jpeg";
+import image1 from "../assets/images/bike2.jpg";
+import { useState } from "react";
 
-export default function App() {
-  const markers = [
-    {
-      id: 1,
-      title: "Sukhdev Vihar",
-      description: "This is Point A",
-      coordinate: {
-        latitude: 28.557180435677136,
-        longitude: 77.27637514029855,
-      },
-    },
-    {
-      id: 2,
-      title: "Jamia Millia Islamia",
-      description: "This is Point B",
-      coordinate: { latitude: 28.56384573530767, longitude: 77.28899141180678 },
-    },
-    {
-      id: 3,
-      title: "Masihgarh",
-      description: "This is Point C",
-      coordinate: { latitude: 28.55876901581252, longitude: 77.27483202162725 },
-    },
-    {
-      id: 4,
-      title: "Pocket A",
-      description: "This is Point D",
-      coordinate: {
-        latitude: 28.555614264513288,
-        longitude: 77.27794470801767,
-      },
-    },
+export default function Task() {
+  const [color, setColor] = useState(false);
 
-    {
-      id: 5,
-      title: "New Friends Colony",
-      description: "This is point E",
-      coordinate: {
-        latitude: 28.568303416468023,
-        longitude: 77.26943502846393,
-      },
-    },
-  ];
-
+  function handleColor() {
+    setColor(!color);
+  }
   return (
-    <View style={styles.container}>
-      {/* <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 28.56384573530767,
-          longitude: 77.28899141180678,
-          latitudeDelta: 2.9,
-          longitudeDelta: 3.89,
-        }}
-      >
-        {markers.map((marker) => (
-          <Marker
-            key={marker.id}
-            coordinate={marker.coordinate}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-        <Polyline
-          coordinates={markers.map((e) => e.coordinate)}
-          strokeColor="black"
-          strokeWidth={4}
-        />
-      </MapView>*/}
-      <Link href="/Tabs">HomeScreen</Link>
+
+
+    <ScrollView>
       
-    </View>
+      <ImageBackground source={image} resizeMode="cover">
+        <ImageBackground source={image1} resizeMode="cover">
+        <Pressable onPress={() => handleColor()}>
+          <Text
+            style={{
+              fontSize: 50,
+              color: "yellow",
+            }}
+          >
+            hello
+          </Text>
+        </Pressable>
+
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            fontWeight: "bold",
+            color: "red",
+          }}
+        >
+          GoodMorning
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 78,
+            fontWeight: "condensed",
+            color: "skyblue",
+          }}
+        >
+          आहिस्ता चल ज़िन्दगी, अभी कई क़र्ज़ चुकाना बाकी है, कुछ दर्द मिटाना बाकी
+          है, कुछ फ़र्ज़ निभाना बाकी है!!. hiii
+        </Text>
+      </ImageBackground>
+      </ImageBackground>
+
+      {color &&{(
+        <ImageBackground source={image} resizeMode="cover"> 
+         <Pressable onPress={()=> handleColor()}>
+          <Text>hello</Text>
+        </Pressable>
+     ) }}
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "red",
+            }}
+          >
+            GoodMorning
+          </Text>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 78,
+              fontWeight: "condensed",
+              color: "skyblue",
+            }}
+          >
+            आहिस्ता चल ज़िन्दगी, अभी कई क़र्ज़ चुकाना बाकी है, कुछ दर्द मिटाना बाकी
+            है, कुछ फ़र्ज़ निभाना बाकी है!!. hiii
+          </Text>
+        </ImageBackground>
+
+    </ScrollView>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    // width: Dimensions.get("window").width,
-    // height: Dimensions.get("window").height,
-  },
-});
