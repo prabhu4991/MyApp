e4ca6806acd903bca8ef34029c61fdd2ed6878ed
prabhu4991 <127617644+prabhu4991@users.noreply.github.com>
2024-07-13 17:39:30 +0530

    the

--- OurApp/app/index.jsx
+++ OurApp/app/index.jsx
@@ -12,6 +12,7 @@ import image1 from "../assets/images/bike2.jpg";
 import image from "../assets/images/photo.jpeg";
 import image2 from "../assets/images/superbike01.jpg";
 import image001 from "../assets/images/newsuper02.jpg";
+import { LinearGradient } from 'expo-linear-gradient';
 export default function Task() {
   const [top, setTop] = useState(0);
   const [left, setLeft] = useState(0);
@@ -19,23 +20,47 @@ export default function Task() {
 
   return (
     <ScrollView>
+      
       <View
         style={{
-          height: 50,
-          width: 50,
-          borderRadius: 50,
+          height: 400,
+          width: 400,
+          borderRadius: 250,
           backgroundColor: "red",
           position: "relative",
+          overflow: "hidden",
+
           top: top,
           left: left,
         }}
-      ></View>
-<Image source={image001}>
+        
+      >
+        <Image source={image001} style={{height:400, width:400}}/>
+      </View>
+<View style={{top:18}}>
+<LinearGradient    
+ colors={["red","blue","green","orange","pink","black"]}    
+style={{
+  height:400,
+  width:400,
+  
+}}
+start={{x:0,y:0}}
+  end={{x:1,y:1}}
+>
+  
+<Text>
+hello
+  </Text>  
+</LinearGradient>
+
+</View>
+
 
 
       <Pressable
         onPress={() => {
-          setTop(top + 5);
+          setTop(top - 5);
         }}
       >
         <Text>Top</Text>
@@ -43,7 +68,7 @@ export default function Task() {
 
       <Pressable
         onPress={() => {
-          setLeft(left + 5);
+          setLeft(left - 5);
         }}
       >
         <Text>Left</Text>
@@ -52,7 +77,7 @@ export default function Task() {
 
       <Pressable
         onPress={() => {
-          setLeft(left - 5);
+          setLeft(left + 5);
         }}
       >
         <Text>Right</Text>
@@ -61,12 +86,12 @@ export default function Task() {
 
       <Pressable
         onPress={() => {
-          setTop(top -5 );
+          setTop(top +5 );
         }}
       >
         <Text>Bottom</Text>
       </Pressable>
-      </Image>
+      
     </ScrollView>
   );
 }
